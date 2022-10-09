ed271b2cfabf5e4f91964dd93cecacf5e73399f0
‘Aries-kob’ <aries-kob@ezweb.ne.jp>
2022-10-09 15:37:50 +0000

    [Update]投稿機能

--- app/controllers/admin/menbers_controller.rb
+++ app/controllers/admin/menbers_controller.rb
@@ -1,13 +1,25 @@
 class Admin::MenbersController < ApplicationController
   def index
+    @menbers = Menber.all
+    
   end
 
   def show
+    @menber = Menber.find(params[:id])
+    @cars = @menber.cars
   end
 
   def edit
+    @menber = Menber.find(params[:id])
   end
 
   def update
+    @menber = Menber.find(params[:id])
+    @menber.update(menber_params)
+    redirect_to admin_menber_path
   end
+  
+  def menber_params
+    params.require(:menber).permit(:name, :nickname, :introduction, :area, :profile_image) 
+  end  
 end
